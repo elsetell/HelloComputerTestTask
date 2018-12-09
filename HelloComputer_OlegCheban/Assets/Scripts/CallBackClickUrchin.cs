0f54de5a098fbaf874904824c8288c752180e1bc
Oleg Cheban <odcheban@gmail.com>
2018-12-09 19:13:57 +0300

    Add animation_urchin

---
+++ HelloComputer_OlegCheban/Assets/Scripts/CallBackClickUrchin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallBackClickUrchin : MonoBehaviour {
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void CallBack()
+    {
+        anim.SetBool("Click", false);
+    }
+}
