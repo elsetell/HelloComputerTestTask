7177909e92814d73fb30c1fb332bedd216bff0dc
Oleg Cheban <odcheban@gmail.com>
2018-12-09 19:08:23 +0300

    Create urchin

---
+++ HelloComputer_OlegCheban/Assets/Scripts/UrchinControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrchinControl : MonoBehaviour {
+    public float minScale;
+    public float maxScale;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            CreateUrchin();
+        }
+    }
+    
+    void CreateUrchin()
+    {
+        Vector2 posSpawn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject urchin = (GameObject)Instantiate(Resources.Load("urchin"), posSpawn, Quaternion.identity);
+        float scaleUrchin = Random.Range(minScale, maxScale);
+        urchin.transform.localScale = new Vector3(scaleUrchin, scaleUrchin, scaleUrchin);
+    }
+}
---
+++ HelloComputer_OlegCheban/Assets/Scripts/UrchinUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrchinUnit : MonoBehaviour {
+    private CircleCollider2D circCol;
+    private float speedRotate;
+
+    void Start()
+    {
+        circCol = GetComponent<CircleCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (CheckGround())
+            RotateToFeet();
+    }
+    bool CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, circCol.radius);
+        return (hit.collider != null && hit.collider.tag == "Ground") ? true : false;
+    }
+    void RotateToFeet()
+    {
+        Vector3 to = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime * speedRotate);
+    }
+
+}
