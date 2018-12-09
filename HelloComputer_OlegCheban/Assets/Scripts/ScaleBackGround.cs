cc9c6c41d4f883d66b86df1b9c943184d61b48a6
Oleg Cheban <odcheban@gmail.com>
2018-12-09 18:53:13 +0300

    Add sprite + backGround.cs

---
+++ HelloComputer_OlegCheban/Assets/Scripts/ScaleBackGround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ScaleBackGround : MonoBehaviour
+{
+    //use sprite for BackGround or UI(StretchMax + Canvas RenderMode:ScSpaceCamera)
+    void Start()
+    {
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+    }
+}
